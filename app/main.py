2611ba359382aafc14b14dae6984fba8df7d3e1e
adityakiran1423 <adi.kiran@gmail.com>
2024-03-20 15:12:01 +0530

    pass 3rd stage

--- app/main.py
+++ app/main.py
@@ -1,10 +1,21 @@
 import socket
 
-
 def main():
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    (clientsocket, address) = server_socket.accept()
-    clientsocket.send(b"HTTP/1.1 200 OK\r\n\r\n")
+    (connection, address) = server_socket.accept()
+    # connection.send(b"HTTP/1.1 200 OK\r\n\r\n")
+    data = socket.recv(4096).decode(encoding='utf-8').slpitlines()
+    # index=data.index("/")
+    # for i in range(index,len(data)):
+    #     if i==" ":
+    #         break
+    # http_path=data[index:i]
+    path=data[0].split(" ")
+    if path[1]=="/":
+        connection.send(b"HTTP/1.1 200 OK\r\n\r\n")
+    else:
+        connection.send(b"HTTP/1.1 404 Not Found\r\n\r\n")
+
 
 
 if __name__ == "__main__":
