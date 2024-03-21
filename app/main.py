31ff67bc5e2ea8761ea33fa4b0543c3f07db0149
adityakiran1423 <adi.kiran@gmail.com>
2024-03-21 11:25:06 +0530

    pass 5th stage

--- app/main.py
+++ app/main.py
@@ -12,11 +12,14 @@ def main():
     content=http_path[6:]
     content_length=len(content)
 
+    _,user_agent=data[2].split(": ")
+    length_user_agent=len(user_agent)
+
     if http_path== "/":
         connection.send(b"HTTP/1.1 200 OK\r\n\r\n")
 
     elif http_path.startswith("/echo/"):
-        data_to_send="HTTP/1.1 200 OK"+"\n"+"Content-Type: text/plain"+"\n"+"Content-Length: "+str(content_length)+"\n"+"\n"+content
+        data_to_send="HTTP/1.1 200 OK"+"\n"+"Content-Type: text/plain"+"\n"+"Content-Length: "+str(length_user_agent)+"\n"+"\n"+user_agent
         connection.sendall(data_to_send.encode())
         #connection.send(f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {content_length}\r\n \n {content}\r\n\r\n")
 
