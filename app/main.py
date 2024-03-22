9a4777baa7c987439f77e22746af220eb8eb2130
adityakiran1423 <adi.kiran@gmail.com>
2024-03-22 13:28:08 +0530

    pass 6th stage

--- app/main.py
+++ app/main.py
@@ -4,52 +4,15 @@ import asyncio
 
 async def main() -> None:
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True) # creating intial server socket
-    (connection, address) = server_socket.accept()
-    task=asyncio.create_task(recieve_data(connection))
+    task=asyncio.create_task(server(server_socket))
+    
+    # server(server_socket)
 
-    # recieve_data(connection)
     await task
-    # data = connection.recv(1024).decode(encoding="utf-8").splitlines()
-
-    # path = data[0].split(" ")  # list containing start line contents
-    # http_path = path[1]
-
-    # content = http_path[6:]
-    # content_length = len(content)
-
-    # _, user_agent = data[2].split(" ")
-    # length_user_agent = len(user_agent)
-
-    # if http_path == "/":
-    #     connection.send(b"HTTP/1.1 200 OK\r\n\r\n")
-
-    # elif http_path.startswith("/echo/"):
-    #     data_to_send = (
-    #         "HTTP/1.1 200 OK"+ "\n"
-    #         + "Content-Type: text/plain"+ "\n"
-    #         + "Content-Length: " + str(content_length)+ "\n\n"
-    #         # + "\n"
-    #         + content+ "\n"
-    #     )
-    #     connection.sendall(data_to_send.encode())
-
-    # elif http_path == "/user-agent":
-    #     data_to_send = (
-    #         "HTTP/1.1 200 OK"+ "\n"
-    #         + "Content-Type: text/plain"+ "\n"
-    #         + "Content-Length: " + str(length_user_agent)+ "\n\n"
-    #         # + "\n"
-    #         + user_agent + "\n"
-    #     )
-    #     connection.sendall(data_to_send.encode())
-
-    # # connection.send(f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {content_length}\r\n \n {content}\r\n\r\n")
-
-    # else:
-    #     connection.send(b"HTTP/1.1 404 Not Found\r\n\r\n")
 
 
-async def recieve_data(connection):
+async def server(server_socket):
+    (connection, address) = server_socket.accept()
     data = connection.recv(1024).decode(encoding="utf-8").splitlines()
     path = data[0].split(" ")  # list containing start line contents
     http_path = path[1]
@@ -87,6 +50,8 @@ async def recieve_data(connection):
 
     else:
         connection.send(b"HTTP/1.1 404 Not Found\r\n\r\n")
+
+    pass
 
 
 if __name__ == "__main__":
