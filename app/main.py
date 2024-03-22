b305650a9c793099a5fd728f5e4e67afb767fa1b
adityakiran1423 <adi.kiran@gmail.com>
2024-03-22 21:44:31 +0530

    pass 6th stage

--- app/main.py
+++ app/main.py
@@ -1,49 +1,44 @@
 import socket
-
-from _thread import *
 import threading
-
-concurrent_sever=threading.Lock()
 
 
 def server(data, connection)->None:
-    while True:
-        path = data[0].split(" ")  # list containing start line contents
-        http_path = path[1]
+    path = data[0].split(" ")  # list containing start line contents
+    http_path = path[1]
 
-        content = http_path[6:]
-        content_length = len(content)
+    content = http_path[6:]
+    content_length = len(content)
 
-        _, user_agent = data[2].split(" ")
-        length_user_agent = len(user_agent)
+    _, user_agent = data[2].split(" ")
+    length_user_agent = len(user_agent)
 
-        if http_path == "/":
-            connection.send(b"HTTP/1.1 200 OK\r\n\r\n")
+    if http_path == "/":
+        connection.send(b"HTTP/1.1 200 OK\r\n\r\n")
 
-        elif http_path.startswith("/echo/"):
-            data_to_send = (
-                "HTTP/1.1 200 OK"+ "\n"
-                + "Content-Type: text/plain"+ "\n"
-                + "Content-Length: " + str(content_length)+ "\n\n"
-                # + "\n"
-                + content+ "\n"
-            )
-            connection.sendall(data_to_send.encode())
+    elif http_path.startswith("/echo/"):
+        data_to_send = (
+            "HTTP/1.1 200 OK"+ "\n"
+            + "Content-Type: text/plain"+ "\n"
+            + "Content-Length: " + str(content_length)+ "\n\n"
+            # + "\n"
+            + content+ "\n"
+        )
+        connection.sendall(data_to_send.encode())
 
-        elif http_path == "/user-agent":
-            data_to_send = (
-                "HTTP/1.1 200 OK"+ "\n"
-                + "Content-Type: text/plain"+ "\n"
-                + "Content-Length: " + str(length_user_agent)+ "\n\n"
-                # + "\n"
-                + user_agent + "\n"
-            )
-            connection.sendall(data_to_send.encode())
+    elif http_path == "/user-agent":
+        data_to_send = (
+            "HTTP/1.1 200 OK"+ "\n"
+            + "Content-Type: text/plain"+ "\n"
+            + "Content-Length: " + str(length_user_agent)+ "\n\n"
+            # + "\n"
+            + user_agent + "\n"
+        )
+        connection.sendall(data_to_send.encode())
 
-        # connection.send(f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {content_length}\r\n \n {content}\r\n\r\n")
+    # connection.send(f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {content_length}\r\n \n {content}\r\n\r\n")
 
-        else:
-            connection.send(b"HTTP/1.1 404 Not Found\r\n\r\n")
+    else:
+        connection.send(b"HTTP/1.1 404 Not Found\r\n\r\n")
 
 
 def main() -> None:
@@ -54,9 +49,8 @@ def main() -> None:
         (connection, address) = server_socket.accept()
         data = connection.recv(1024).decode(encoding="utf-8").splitlines()
 
-        concurrent_sever.acquire()
-        start_new_thread(server, (data,connection,))
-
+        thread = threading.Thread(target=server, args=(data, connection))
+        thread.start()
 
 
 if __name__ == "__main__":
