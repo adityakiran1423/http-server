dd976b5fc10f98b3def1008de30320eeffa59ae8
adityakiran1423 <adi.kiran@gmail.com>
2024-03-24 00:50:40 +0530

    pass 7th stage

--- app/main.py
+++ app/main.py
@@ -63,12 +63,12 @@ def server(connection)->None:
     #             response = f"HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: {len(file_content)}\r\n\r\n{file_content}\r\n"
 
     #     connection.sendall(response.encode())
-    elif path.startswith("/files/"):
+    elif http_path.startswith("/files/"):
         directory = ""
         if sys.argv[1] == "--directory":
             directory = sys.argv[2]
 
-        filename = path[len("/files/") :]
+        filename = http_path[len("/files/") :]
         file_path = os.path.join(directory, filename)
 
         response = "HTTP/1.1 404 Not Found \r\n\r\n"
@@ -77,7 +77,7 @@ def server(connection)->None:
             with open(file_path, "r") as file:
                 fileContent = file.read()
                 response = f"HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: {len(fileContent)}\r\n\r\n{fileContent}\r\n"
-                
+
         print(response)
         connection.send(response.encode())
         
